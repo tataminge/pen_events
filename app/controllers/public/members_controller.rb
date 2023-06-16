a4f0eae2ca4432abdb96e6d87986ce3bc6d9d92d
tataminge <tatatatami0818@gmail.com>
2023-06-16 21:21:18 +0000

    add member_view

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,7 +1,24 @@
 class Public::MembersController < ApplicationController
+    before_action :authenticate_member!
+    before_action :set_member, only: [:favorites]
+
   def show
   end
 
   def edit
   end
+
+  def index
+    favorites = Favorite.where(member_id: @member.id).pluck(:post_event_id)
+    @favorite_posts = Post_event.find(favorites)
+  end
+
+
+  private
+
+  def set_member
+    @member = Member.find(params[:id])
+  end
+
+
 end
